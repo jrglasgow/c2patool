161ec7c0e928170a5ab9b911194afea294d83185
James R Glasgow <james.glasgow@associates.hq.dhs.gov>
2024-01-25 15:14:06 -0700

    added Jrglasgow\C2paWrapper\Tool::SIGNATURE_ALGORITHMS which lists the allowed algorithms

--- src/Tool.php
+++ src/Tool.php
@@ -8,6 +8,22 @@ use Psr\Log\LoggerInterface;
 class Tool implements LoggerAwareInterface {
 
   /**
+   * The signature types allowed - see https://opensource.contentauthenticity.org/docs/manifest/signing-manifests#signature-types
+   */
+  const SIGNATURE_ALGORITHMS = [
+    'ecdsa-with-SHA256',
+    'ecdsa-with-SHA384',
+    'ecdsa-with-SHA512',
+    'sha256WithRSAEncryption',
+    'sha384WithRSAEncryption',
+    'sha512WithRSAEncryption',
+    'prime256v1',
+    'secp384r1',
+    'secp521r1',
+    'id-Ed25519',
+  ];
+
+  /**
    * @var \Psr\Log\LoggerInterface
    */
   protected LoggerInterface $logger;
